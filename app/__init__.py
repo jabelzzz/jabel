8e7b2df739e60c27427251222c333d69974fbe76
Jabel <jabel.alvarez@zucchetti.com>
2025-07-07 15:46:34 +0100

    feat: add wave effect to the hand emoji and add some requirements to the requirements.txt file

--- app/__init__.py
+++ app/__init__.py
@@ -2,8 +2,13 @@ from flask import Flask
 import os
 
 def create_app():
-    app = Flask(__name__, template_folder=os.path.join(os.path.dirname(os.path.dirname(__file__)), 'templates'))
-    # Registrar rutas
+    template_dir = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'templates')
+    static_dir = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'static')
+    
+    app = Flask(__name__, 
+               template_folder=template_dir,
+               static_folder=static_dir)
+    
     from app import routes
     routes.init_app(app)
     return app
